a00bbd41618962e24d3a06a2f89aecfbe6ab4e75
Adam Jędrzejec <adamjedrzejec@gmail.com>
2019-11-16 01:11:31 +0100

    [fix] changed links so they do not redirect

--- app/javascript/packs/page_header/navbars.jsx
+++ app/javascript/packs/page_header/navbars.jsx
@@ -11,26 +11,26 @@ export default class Navbars extends React.Component {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="strona-glowna">Strona główna</Nav.Link>
-              <Nav.Link href="aktualnosci">Aktualności</Nav.Link>
+              <Nav.Link href="#strona-glowna">Strona główna</Nav.Link>
+              <Nav.Link href="#aktualnosci">Aktualności</Nav.Link>
               <NavDropdown title="O szkole" id="about-school-dropdown">
-                <NavDropdown.Item href="o-szkole/historia">Historia szkoły</NavDropdown.Item>
-                <NavDropdown.Item href="o-szkole/programy-i-projekty">Programy i projekty</NavDropdown.Item>
-                <NavDropdown.Item href="o-szkole/galeria-zdjec">Galeria zdjęć</NavDropdown.Item>
-                <NavDropdown.Item href="o-szkole/rada-rodzicow">Rada Rodziców</NavDropdown.Item>
-                <NavDropdown.Item href="o-szkole/grono-pedagogiczne">Grono pedagogiczne</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/historia">Historia szkoły</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/programy-i-projekty">Programy i projekty</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/galeria-zdjec">Galeria zdjęć</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/rada-rodzicow">Rada Rodziców</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/grono-pedagogiczne">Grono pedagogiczne</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="o-szkole/informacje-rady-rodzicow">Informacje Rady Rodziców</NavDropdown.Item>
+                <NavDropdown.Item href="#o-szkole/informacje-rady-rodzicow">Informacje Rady Rodziców</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Dla ucznia" id="for-students-dropdown">
-                <NavDropdown.Item href="dla-ucznia/plan-lekcji">Plan lekcji</NavDropdown.Item>
+                <NavDropdown.Item href="#dla-ucznia/plan-lekcji">Plan lekcji</NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Dla rodziców" id="for-parents-dropdown">
-                <NavDropdown.Item href="dla-rodzicow/wazne-dokumenty">Ważne dokumenty</NavDropdown.Item>
-                <NavDropdown.Item href="dla-rodzicow/porady-pedagoga-szkolnego">Porady pedagoga szkolnego</NavDropdown.Item>
-                <NavDropdown.Item href="dla-rodzicow/informacje-dla-rodzicow">Informacje dla rodziców</NavDropdown.Item>
+                <NavDropdown.Item href="#dla-rodzicow/wazne-dokumenty">Ważne dokumenty</NavDropdown.Item>
+                <NavDropdown.Item href="#dla-rodzicow/porady-pedagoga-szkolnego">Porady pedagoga szkolnego</NavDropdown.Item>
+                <NavDropdown.Item href="#dla-rodzicow/informacje-dla-rodzicow">Informacje dla rodziców</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="kontakt">Kontakt</Nav.Link>
+              <Nav.Link href="#kontakt">Kontakt</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
